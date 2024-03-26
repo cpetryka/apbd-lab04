bdd15ee3d5652fce1b88a147209b212a5e81df1d
Cezary Petryka <cezaryp10@gmail.com>
2024-03-26 14:50:16 +0100

    Add first unit test

---
+++ zadanie/LegacyApp.Tests/UserServiceTests.cs
@@ -0,0 +1,24 @@
+namespace LegacyApp.Tests;
+
+public class UserServiceTests
+{
+    [Fact]
+    public void AddUser_ReturnsFalseWhenFirstNameIsEmpty()
+    {
+        // Arrange
+        var userService = new UserService();
+
+        // Act
+        var result = userService.AddUser(
+            null,
+            "Kowalski",
+            "kowalski@kowal.com",
+            DateTime.Parse("2000-01-01"),
+            1
+        );
+
+        // Assert
+        // Assert.Equal(false, result);
+        Assert.False(result);
+    }
+}
